892f20a061ff8cd18683d6d26c23ef35d9e53478
kpitlawar <pitlawarkanchan@gmail.com>
2023-12-21 15:14:44 +0530

    Added content in Chef's corner page and first draft of product page

--- src/components/Pages/Products.js
+++ src/components/Pages/Products.js
@@ -2,5 +2,35 @@ import React from "react";
 import "../../App.css";
 
 export default function Products() {
-  return <h1 className="products">PRODUCTS</h1>;
+
+  const menuItems = [
+    'img-4.jpeg',
+    'img-4.jpg',
+    'img-5.jpeg',
+    'img-8.jpg',
+    'img-1.jpeg',
+    'img-2.jpeg',
+    'img-3.jpeg'
+    // Add more menu item images as needed
+  ];
+
+   
+
+  return (
+    <div>
+      <div className="menu-gallery">
+        <h2>Our Delicious Menu</h2>
+        <div className="image-container">
+          {menuItems.map((item, index) => (
+            <div className="image-card">
+            <img key={index} 
+            // eslint-disable-next-line
+            src={'images' + '/' + item}
+            alt={`Menu Item ${index + 1}`} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/components/Pages/Services.js
+++ src/components/Pages/Services.js
@@ -3,13 +3,29 @@ import "../../App.css";
 
 export default function Services() {
   return (
-    <>
-      <div className="navbar-container">
-        <h1>Chef's Corner</h1>
+    <div className="chef-container">
+      <div className="chef-image">
+        <img src="images/foodmaplogo.png" alt="Chef" />
       </div>
-      <div className="img-div">
-        <img src="images/foodmaplogo.png" alt="Logo"></img>
+      <div className="chef-journey">
+
+        <h2>Welcome to Chef's Corner!</h2>
+        <p>
+          I'm Kanchan Pitlawar, a seasoned software engineer with four years of experience in the tech world. Beyond the lines of code and the fast-paced tech landscape, I discovered an unexpected passion for the culinary arts. In 2021, I decided to blend my love for coding with my newfound enthusiasm for cooking, giving birth to my Instagram page – a virtual kitchen where I share not just recipes, but the stories behind each dish.
+        </p>
+        <p>
+          My culinary journey is a fusion of flavors, techniques, and a continuous quest for innovation. From crafting pixel-perfect code to perfecting the art of plating, I've found that the attention to detail in software engineering seamlessly translates to the precision required in the kitchen. Each dish is a unique experiment, a blend of creativity and methodical execution.
+        </p>
+        <p>
+          Beyond the delicious snapshots, Chef's Corner is a community where tech enthusiasts and foodies converge. My Instagram page serves as a canvas to share the joy, challenges, and breakthroughs that come with both worlds. Moreover, for those interested in the professional side of my journey, my LinkedIn page is a hub for discussions on the intersection of technology and culinary arts.
+        </p>
+        <p>
+          Join me on this exciting adventure as we explore the synergy between coding and cooking, unraveling the layers of flavors in both the digital and culinary realms. Let's savor the journey together!
+        </p>
+        <p>
+          Connect with me on <a href="https://www.linkedin.com/company/foodmappune/" target="_blank" rel="noopener noreferrer">LinkedIn</a> for a deeper dive into my professional journey and broader insights.
+        </p>
       </div>
-    </>
+    </div>
   );
 }
